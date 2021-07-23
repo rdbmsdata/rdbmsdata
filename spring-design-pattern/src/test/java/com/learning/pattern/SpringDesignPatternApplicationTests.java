973f46f336b082dcf6a50201f37d0dcca91fbe35
Ranjan <rdbmsdata.ranjan@gmail.com>
2021-07-23 14:37:14 -0400

    Desigh Pattern folder

---
+++ spring-design-pattern/src/test/java/com/learning/pattern/SpringDesignPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.pattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDesignPatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
